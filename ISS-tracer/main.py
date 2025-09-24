bc955606575331ee481478461c45afaa5c358a81
MBU <tb.uzunkaya@gmail.com>
2025-09-24 16:32:57 +0200

    Add ISS-tracer as normal folder

---
+++ ISS-tracer/main.py
@@ -0,0 +1,48 @@
+import smtplib
+import time
+import requests
+from datetime import datetime
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+
+    data = response.json()
+    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+
+    MY_LAT = 52.391043
+    MY_LONG = 4.842928
+
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0
+    }
+
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params= parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(user="email", password="app-password")
+        connection.sendmail(
+            from_addr= "email",
+            to_addrs= "receiver",
+            msg="Subject: Look Up \n\n The ISS is above you in the sky."
+
+        )
+
